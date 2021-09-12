05b5a31b3e2d9809096c2045e10b0381d72d7013
Arslan Jan Soomro <arslanjs.dev@gmail.com>
2021-09-12 05:59:48 -0700

    Initial Setup With Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'dela' : ['Dela Gothic One', 'cursive'],
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'poppins' : ['Poppins', 'sans-serif']
+      },
+      colors: {
+        'cu-black' : '#0C1618',
+        'cu-green' : '#70F8BA',
+        'cu-blue' : '#3772FF',
+        'cu-white' : '#FFFCFF',
+        'cu-gray' : '#EEEEEE'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
