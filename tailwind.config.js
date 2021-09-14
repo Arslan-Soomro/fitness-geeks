95c9f55b0419497f1cab1c97c4a122f49c5d2366
Arslan Jan Soomro <arslanjs.dev@gmail.com>
2021-09-14 00:21:16 -0700

    Add xs breakpoint and set container center true

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,33 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      //That shorthand version of adding a small breakpoint produces an error
+
+      'xs': '450px',
+      // => @media (min-width: 450px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    container : {
+      center: true,
+      screens: {
+        '2xl' : '1280px',
+      },
+    },
     extend: {
       fontFamily: {
         'dela' : ['Dela Gothic One', 'cursive'],
